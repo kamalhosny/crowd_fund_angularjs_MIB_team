b4ebb74d81d2917f0b917d5929f692a5b5e8694a
kamalhosny <kamal-aldin-hosny@hotmail.com>
2017-05-25 13:46:31 +0200

    fix some symantic errors

--- src/app/campaigns/campaign.js
+++ src/app/campaigns/campaign.js
@@ -1,27 +1,17 @@
-angular.module('crowdFundApp').controller('campaignController', function($scope, $http, $stateParams,$sce) {
-  $http.get('http://localhost:3000/campaign/'+$stateParams.id+'.json').then(
-    function(success){
-      $scope.campaign=success.data
-      console.log($scope.campaign);
-      ////////////
-      // $scope.campaign.video="https://youtube.com/aqAUmgE3WyM"
-      // $scope.campaign.achieved=3;
-      ///////////////
-      if($scope.campaign.video){
-        $scope.youTubeVideoUrl=$scope.campaign.video.replace('.com/watch?v=',".com/embed/");
-        $scope.video=$sce.trustAsResourceUrl($scope.youTubeVideoUrl);
+angular.module('crowdFundApp').controller('campaignController', function($scope, $http, $stateParams, $sce) {
+  $http.get('http://localhost:3000/campaign/' + $stateParams.id + '.json').then(
+    function(success) {
+      $scope.campaign = success.data
+
+      if ($scope.campaign.video) {
+        $scope.youTubeVideoUrl = $scope.campaign.video.replace('.com/watch?v=', ".com/embed/");
+        $scope.video = $sce.trustAsResourceUrl($scope.youTubeVideoUrl);
       }
 
-      // $scope.achieved=$scope.campaign.achieved;
-      $scope.achievedPercentage=String($scope.campaign.achieved*100/$scope.campaign.goal)+'%';
+      $scope.achieved=$scope.campaign.achieved;
+      $scope.achievedPercentage = String($scope.campaign.achieved * 100 / $scope.campaign.goal) + '%';
     },
-    function(err){
+    function(err) {
       console.log(err);
-    }
-  )
-
-  //
-  // // console.log($scope.achieved);
-  //
-  //
+    });
 });
