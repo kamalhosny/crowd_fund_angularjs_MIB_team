adf3ff3e0acddc5e856a7d4a80274e8358312e35
Saleh <salehaly01@gmail.com>
2017-05-30 12:09:03 +0200

    Add user profile view, edit and delete

---
+++ src/app/controllers/users/user_profile_show.js
@@ -0,0 +1,21 @@
+angular.module('crowdFundApp').controller('userShow',
+function($scope, UserService, toaster, $sce, $auth, $stateParams,$location) {
+  $scope.returnGender = function(gender) {
+    if (gender == true) {
+      return 'Male';
+    }
+    return 'Female';
+  }
+
+  UserService.getUser().then(function(success) {
+    $scope.user = success.data;
+  }, function(error) {
+    toaster.pop('error', 'Failed to get user data!')
+  });
+
+  $scope.removeUser = function() {
+    UserService.deleteUser()
+    $location.url('/sign_in');
+    toaster.pop('error', 'Profile was deleted, we will miss you! :()')
+  }
+});
